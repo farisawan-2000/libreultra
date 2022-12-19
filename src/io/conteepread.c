21c7277aaa5ec3e727770bdda8890733dfb512e3
someone2639 <someone2639@gmail.com>
2022-12-18 19:06:38 -0500

    some osEepromRead fixes

--- src/io/conteepread.c
+++ src/io/conteepread.c
@@ -4,99 +4,79 @@
 
 static void __osPackEepReadData(u8 address);
 OSPifRam __osEepPifRam;
-s32 osEepromRead(OSMesgQueue *mq, u8 address, u8 *buffer)
-{
-	s32 ret;
-	int i;
-	u16 type;
-	u8 *ptr;
-	OSContStatus sdata;
-	__OSContEepromFormat eepromformat;
-	ret = 0;
-	i = 0;
-	ptr = (u8 *)&__osEepPifRam.ramarray;
-	__osSiGetAccess();
-	ret = __osEepStatus(mq, &sdata);
-	type = sdata.type & (CONT_EEPROM | CONT_EEP16K);
+s32 _osEepromRead(OSMesgQueue* arg0, u8 address, u8* buffer) {
+    s32 ret;
+    s32 i;
+    u16 type;
+    u8 *ptr;
+    OSContStatus sdata;
+    __OSContEepromFormat eepromformat;
 
-	if (ret != 0)
-	{
-		__osSiRelAccess();
-		return CONT_NO_RESPONSE_ERROR;
-	}
-	switch (type)
-	{
-	case CONT_EEPROM:
-		if (address > EEPROM_MAXBLOCKS)
-		{
-			__osSiRelAccess();
-			return -1;
-		}
+    ptr = (u8*)&__osEepPifRam.ramarray;
+    __osSiGetAccess();
+    ret = __osEepStatus(arg0, &sdata);
+    if (ret == 0) {
+        type = sdata.type & 0xC000;
 
-		break;
-	case CONT_EEPROM | CONT_EEP16K:
-		if (address > EEP16K_MAXBLOCKS) //not technically possible
-		{
-			__osSiRelAccess();
-			return -1;
-		}
-		break;
-	default:
-		__osSiRelAccess();
-		return CONT_NO_RESPONSE_ERROR;
-		break;
-	}
-	while (sdata.status & CONT_EEPROM_BUSY)
-	{
-		__osEepStatus(mq, &sdata);
-	}
-	__osPackEepReadData(address);
-	ret = __osSiRawStartDma(OS_WRITE, &__osEepPifRam); //send command to pif
-	osRecvMesg(mq, NULL, OS_MESG_BLOCK);
-	ret = __osSiRawStartDma(OS_READ, &__osEepPifRam); //recv response
-	__osContLastCmd = CONT_CMD_READ_EEPROM;
-	osRecvMesg(mq, NULL, OS_MESG_BLOCK);
-	for (i = 0; i < 4; i++) //skip the first 4 bytes
-	{
-		ptr++;
-	}
-	eepromformat = *(__OSContEepromFormat *)ptr;
-
-	ret = CHNL_ERR(eepromformat);
-
-	if (ret == 0)
-		for (i = 0; i < ARRLEN(eepromformat.data); i++)
-			*buffer++ = eepromformat.data[i];
-
-	__osSiRelAccess();
-	return ret;
+        switch (type) {
+            case 0x8000:
+                if (address >= 0x40) {
+                    ret = -1;
+                }
+                break;
+            case 0xC000:
+                if (address >= 0x100) {
+                    ret = -1;
+                }
+                break;
+            default:
+                ret = 8;
+                break;
+            
+        }
+    }
+    if (ret != 0) {
+        __osSiRelAccess();
+    } else {
+        while (sdata.status & 0x80) {
+            __osEepStatus(arg0, &sdata);
+        }
+        __osPackEepReadData(address);
+        __osSiRawStartDma(1, &__osEepPifRam);
+        osRecvMesg(arg0, NULL, 1);
+        __osSiRawStartDma(0, &__osEepPifRam);
+        __osContLastCmd = 4;
+        osRecvMesg(arg0, NULL, 1);
+        for (i = 0; i < 4; i++) {
+            ptr += 1;
+        }
+        eepromformat = *(__OSContEepromFormat *)ptr;
+        
+        ret = CHNL_ERR(eepromformat);
+        if (ret == 0) {
+    		for (i = 0; i < ARRLEN(eepromformat.data); i++) {
+    			*buffer++ = eepromformat.data[i];
+    		}
+    	}
+        __osSiRelAccess();
+    }
+    return ret;
 }
 
-static void __osPackEepReadData(u8 address)
-{
-	u8 *ptr;
-	__OSContEepromFormat eepromformat;
-	int i;
-	ptr = (u8 *)&__osEepPifRam.ramarray;
-	for (i = 0; i < ARRLEN(__osEepPifRam.ramarray); i++)
-	{
-		__osEepPifRam.ramarray[i] = CONT_CMD_NOP;
-	}
-	__osEepPifRam.pifstatus = CONT_CMD_EXE;
+static void __osPackEepReadData(u8 address) {
+    u8 *ptr = (u8*)&__osEepPifRam.ramarray;
+    __OSContEepromFormat eepromformat;
+    int i;
 
-	eepromformat.txsize = CONT_CMD_READ_EEPROM_TX;
+    __osEepPifRam.pifstatus = 1;
+    eepromformat.txsize = CONT_CMD_READ_EEPROM_TX;
 	eepromformat.rxsize = CONT_CMD_READ_EEPROM_RX;
 	eepromformat.cmd = CONT_CMD_READ_EEPROM;
 	eepromformat.address = address;
-	for (i = 0; i < ARRLEN(eepromformat.data); i++)
-	{
-		eepromformat.data[i] = 0;
-	}
-	for (i = 0; i < 4; i++) //skip the first 4 bytes
-	{
-		*ptr++ = 0;
-	}
-	*(__OSContEepromFormat *)(ptr) = eepromformat;
-	ptr += sizeof(__OSContEepromFormat);
-	ptr[0] = CONT_CMD_END;
+
+    for (i = 0; i < MAXCONTROLLERS; i++) {
+        *ptr++ = 0;
+    }
+    *(__OSContEepromFormat *)(ptr) = eepromformat;
+    ptr[0] = CONT_CMD_END;
 }
