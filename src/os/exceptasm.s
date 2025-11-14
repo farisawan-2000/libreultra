ef3a2464909fc56403c1112db4b24c2e66e950b0
someone2639 <someone2639@gmail.com>
2025-11-13 22:54:27 -0500

    uncomment vecs

--- src/os/exceptasm.s
+++ src/os/exceptasm.s
@@ -1,11 +1,10 @@
 #include "asm_helper.h"
 #include "exceptasm.h"
 #include <os.h>
-/*  TODO: put this into kirby 64s .data section
 .set gp=64
 
 
-.data
+.rodata
 
 EXPORT(__osHwIntTable)
 .word 0x0, 0x0, 0x0, 0x0, 0x0
@@ -55,7 +54,7 @@ EXPORT(__osIntOffTable)
 .byte CART 
 EXPORT(__osIntTable)
 .word redispatch, sw1, sw2, rcp, cart, prenmi, IP6_Hdlr, IP7_Hdlr, counter
-*/
+
 .text
 
 LEAF(__osExceptionPreamble)
